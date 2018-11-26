a95ef17b65b78db615872ce5776945af49f57ed4
Jason Kuo <jsnkuo7@gmail.com>
2018-11-25 19:34:15 -0800

    Start adding types

--- Type.java
+++ Type.java
@@ -2,24 +2,25 @@ import java.awt.Color;
 import java.util.*;
 
 public enum Type {
-    NORMAL(new Color(165,42,42), new HashSet<>(Arrays.asList()),
-            new HashSet<>(Arrays.asList())),
+    NORMAL(new Color(105,42,22), new HashSet<>(Arrays.asList()),
+            new HashSet<>(Arrays.asList()), new HashSet<>(Arrays.asList())),
     FIRE (Color.RED, new HashSet<>(Arrays.asList("GRASS")),
-            new HashSet<>(Arrays.asList("FIRE", "WATER"))),
+            new HashSet<>(Arrays.asList("FIRE", "WATER")), new HashSet<>(Arrays.asList())),
     WATER (Color.BLUE, new HashSet<>(Arrays.asList("FIRE")),
-            new HashSet<>(Arrays.asList("GRASS", "WATER"))),
+            new HashSet<>(Arrays.asList("GRASS", "WATER")), new HashSet<>(Arrays.asList())),
     ELECTRIC (new Color(233,189,25), new HashSet<>(Arrays.asList("WATER")),
-            new HashSet<>(Arrays.asList("ELECTRIC", "GRASS"))),
+            new HashSet<>(Arrays.asList("ELECTRIC", "GRASS")), new HashSet<>(Arrays.asList())),
     GRASS (new Color(0,127,0), new HashSet<>(Arrays.asList("WATER")),
-            new HashSet<>(Arrays.asList("FIRE", "GRASS")));
+            new HashSet<>(Arrays.asList("FIRE", "GRASS")), new HashSet<>(Arrays.asList()));
 
-    private Set<String> strong, weak;
+    private Set<String> strong, weak, immune;
     private Color color;
 
-    Type(Color c, Set<String> s, Set<String> w){
+    Type(Color c, Set<String> s, Set<String> w, Set<String> i){
         color = c;
         strong = s;
         weak = w;
+        immune = i;
     }
 
     Color getColor(){
@@ -27,7 +28,9 @@ public enum Type {
     }
 
     double multiplierAgainst(Type other){
-        if(strong.contains(other.name())){
+        if(immune.contains(other.name())){
+            return 0.5;
+        } else if(strong.contains(other.name())){
             return 1.4;
         } else if(weak.contains(other.name())){
             return 0.7;
